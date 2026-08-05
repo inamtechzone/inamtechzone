3222ec0fc796e95409757975b2e15aad70d0422c
inamtechzone <inamtechzone@gmail.com>
2026-08-05 09:27:52 +0500

    Clean up config.js by removing comments
    Removed comments and retained configuration settings.

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,8 +1,3 @@
-/**
- * config.js
- * Single place to point the static frontend at your deployed Apps Script Web App.
- * After you deploy Code.gs (Deploy > New deployment > Web app), paste the URL below.
- */
 window.ITZ_CONFIG = {
   API_URL: "https://script.google.com/macros/s/AKfycbxuKOpTYh5BtpO20atki3kQGsg84uB7R-IfcNICwiVeOPFg5_FUzVdbStkWyJbQ8wh8Hw/exec",
   STORE_NAME: "INAM TECH ZONE",
