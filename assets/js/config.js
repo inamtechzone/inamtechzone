03932c7386fdeb38178a76635439ac45bcca305c
inamtechzone <inamtechzone@gmail.com>
2026-08-05 05:22:52 +0500

    Change API_URL to new deployment endpoint
    Updated API_URL to point to the new deployment ID.

--- assets/js/config.js
+++ assets/js/config.js
@@ -4,7 +4,7 @@
  * After you deploy Code.gs (Deploy > New deployment > Web app), paste the URL below.
  */
 window.ITZ_CONFIG = {
-  API_URL: "https://script.google.com/macros/s/AKfycbxuKOpTYh5BtpO20atki3kQGsg84uB7R-IfcNICwiVeOPFg5_FUzVdbStkWyJbQ8wh8Hw/exec",
+  API_URL: "https://script.google.com/macros/s/AKfycbxuKOpTYh5BtpO20atki3kQGsg84uB7R-IfcNICwiVeOPFg5_FUzVdbStkWyJbQ8wh8Hw/exec"https://script.google.com/macros/s/APKA-DEPLOYMENT-ID/exec?action=image.serve&id=KISI-BHI-IMAGE-KA-FILE-ID,
   STORE_NAME: "INAM TECH ZONE",
   WHATSAPP_NUMBER: "923001234567", // overridden at runtime by Settings once loaded
   CURRENCY: "Rs",
