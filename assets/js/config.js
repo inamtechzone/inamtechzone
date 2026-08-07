631bec99489f8fed6f4234e0b9dc4abc2a28a67b
inamtechzone <inamtechzone@gmail.com>
2026-08-07 07:53:50 -0700

    Refactor configuration settings in config.js

--- assets/js/config.js
+++ assets/js/config.js
@@ -2,12 +2,11 @@
 window.ITZ_CONFIG = {
   API_URL: "https://script.google.com/macros/s/AKfycbxuKOpTYh5BtpO20atki3kQGsg84uB7R-IfcNICwiVeOPFg5_FUzVdbStkWyJbQ8wh8Hw/exec",
   STORE_NAME: "INAM TECH ZONE",
-  WHATSAPP_NUMBER: "923001234567", // overridden at runtime by Settings once loaded
+  WHATSAPP_NUMBER: "923001234567",
   CURRENCY: "Rs",
 };
 
-// Aliases & Global Variables (Taake baqi tamam pages bina error ke kaam karein)
+// Aliases (Attaching directly to window prevents 'already declared' SyntaxErrors)
+window.ITZ = window.ITZ_CONFIG;
 window.CONFIG = window.ITZ_CONFIG;
 window.API_URL = window.ITZ_CONFIG.API_URL;
-var API_URL = window.ITZ_CONFIG.API_URL;
-var CONFIG = window.ITZ_CONFIG;
